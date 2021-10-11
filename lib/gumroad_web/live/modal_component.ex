ad65b90cac19eec269c5de6e289b95b43220ed0f
Akash <aakash@hey.com>
2021-10-11 16:16:43 +0530

    add basic styles

--- lib/gumroad_web/live/modal_component.ex
+++ lib/gumroad_web/live/modal_component.ex
@@ -4,18 +4,23 @@ defmodule GumroadWeb.ModalComponent do
   @impl true
   def render(assigns) do
     ~H"""
-    <div
+    <div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true"
       id={@id}
-      class="phx-modal"
       phx-capture-click="close"
       phx-window-keydown="close"
       phx-key="escape"
       phx-target={@myself}
-      phx-page-loading>
+      phx-page-loading
+    >
+      <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true" phx-capture-click="close" phx-target={@myself}></div>
 
-      <div class="phx-modal-content">
-        <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
-        <%= live_component @component, @opts %>
+        <!-- This element is to trick the browser into centering the modal contents. -->
+        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+        <div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+          <%= live_component @component, @opts %>
+        </div>
       </div>
     </div>
     """
