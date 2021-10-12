a52381f1da29c42e0a357c973f5b90d50083dfab
Akash <aakash@hey.com>
2021-10-13 00:10:13 +0530

    update releases config

--- config/runtime.exs
+++ config/runtime.exs
@@ -41,7 +41,9 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: String.to_integer(System.get_env("PORT") || "4000")
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    server: true,
+    url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
 
   # ## Using releases
   #
