ae73d407c726b665406f28753089dbe3099e8f55
Akash <aakash@hey.com>
2021-10-12 20:22:56 +0530

    add validation to rating

--- lib/gumroad/products/review.ex
+++ lib/gumroad/products/review.ex
@@ -15,5 +15,6 @@ defmodule Gumroad.Products.Review do
     review
     |> cast(attrs, [:description, :rating, :product_id])
     |> validate_required([:description, :rating, :product_id])
+    |> validate_inclusion(:rating, [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0])
   end
 end
